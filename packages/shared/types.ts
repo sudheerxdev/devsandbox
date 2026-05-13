98c9a53e1b76ad11531fcd7b7dcb8f671595a2ba
sudheerxdev <sudheer01yadav2005@gmail.com>
2026-05-13 11:47:09 +0530

    added new line in file

--- packages/shared/types.ts
+++ packages/shared/types.ts
@@ -1,11 +1,10 @@
-// API Types
+
 export interface ApiResponse<T> {
   data: T;
   status: string;
   message?: string;
 }
 
-// Common types that will be shared across frontend and backend
 export interface User {
   id: string;
   email: string;
